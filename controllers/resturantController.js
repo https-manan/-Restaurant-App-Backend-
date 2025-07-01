a14194c68afd8b5b6d9a9241449abc10fc251a19
Manan <Mananbhardwaj2705@gmail.com>
2025-07-01 06:03:10 +0530

    Added functionality for resturant(CRUD)

--- controllers/resturantController.js
+++ controllers/resturantController.js
@@ -1,4 +1,5 @@
-const resturantMode = require("../models/resturantMode");
+const { success } = require("zod/v4");
+const resturantModel = require("../models/resturantModel");
 
 const createResturantController = async (req,res)=>{
     try {
@@ -9,17 +10,97 @@ const createResturantController = async (req,res)=>{
                 message:'Fields are require'
             })
         }
-        const newResturant = new resturantMode({title,imageUrl,foods,pickUp,delivery,isOpen,rating,ratingCount,code,coords});
+        const newResturant = new resturantModel({title,imageUrl,foods,pickUp,delivery,isOpen,rating,ratingCount,code,coords});
         await newResturant.save(); 
     } catch (error) {
-       console.log(error);
-       res.status(500).send({
+    console.log(error);
+    res.status(500).send({
         success:false,
         message:'Error in creating API'
-       }) 
+    }) 
     }
 }
 
+const getAllResturantController =async (req,res)=>{
+    try {
+        const resturants = await resturantModel.find({}) //Here we have to return all resturants Found
+        if(!resturants){
+            res.status(404).send({
+                success:false,
+                message:'Resturant not available'
+            })
+        }res.status(200).send({
+            success:true,
+            totalCount:resturants.length,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Issue in getResturant API",
+        })
+    }
+}
+
+const getResturantByIDController = async (req,res)=>{
+    try {
+    const resturant = await resturantModel.findById(req.params.id);
+    if (!resturant) {
+        return res.status(404).send({
+            success: false,
+            message: 'resturant does not exist in DB',
+        });
+    }else{
+        res.status(200).send({
+            success:true,
+            resturant
+        })
+    }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'Error in getting API'
+        })
+    }
+}
+
+const deleteResturantController = async (req, res) => {
+  try {
+    const resturantId = req.params.id;
+
+    if (!resturantId) {
+      return res.status(404).send({
+        success: false,
+        message: 'No Restaurant Found OR Provide Restaurant ID',
+      });
+    }
+
+    const deletedResturant = await resturantModel.findByIdAndDelete(resturantId);
+
+    if (!deletedResturant) {
+      return res.status(404).send({
+        success: false,
+        message: 'No Restaurant Found with This ID',
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: 'Restaurant Deleted Successfully',
+      data: deletedResturant,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: 'Error in delete restaurant API',
+      error,
+    });
+  }
+};
 
 
-module.exports = {createResturantController}
+module.exports = {createResturantController,getAllResturantController,getResturantByIDController,deleteResturantController}
--- routes/resturantRoutes.js
+++ routes/resturantRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const routes = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
-const { createResturantController } = require('../controllers/resturantController');
+const { createResturantController, getAllResturantController, getResturantByIDController, deleteResturantController } = require('../controllers/resturantController');
 
 
 
 //CREATE RESTURANT
 routes.post('/create',authMiddleware,createResturantController)
-
-
+//GET ALL RESTURANTS 
+routes.get('/getAll',getAllResturantController)
+//GET RESTURANT BY ID
+routes.get('/get/:id',getResturantByIDController);
+//DELETE RESTURANT 
+routes.delete('/delete',authMiddleware,deleteResturantController)
 
 module.exports = routes;
